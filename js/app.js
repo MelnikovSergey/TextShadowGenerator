3a4afa151348d4148629f325d6929290dbe041bb
Sergey Melnikov <39922197+MelnikovSergey@users.noreply.github.com>
2019-03-13 23:45:02 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-
+function cssShadow({
+	font_size,
+	offset_x,
+	offset_y,
+	blut,
+	opacity,
+	color,
+	rgba
+}) {
+	var cssStyles = offset_x + 'px ', offset_y + 'px ', + blur + 'px ' + rgba;
+	$('h1').css('text-shadow', cssStyles);
+	$('#resultHex').val('background-color: ' + color + ';\nopacity: ' + opacity + '\nfont-size: ' + font_size + 'px;' );
+	$('#resultRgba').val('text-shadow: ' + offset_x + 'px '  + offset_y + 'px ' + blur + 'px ' + rgba + ';\nfont-size: ' + font_size + 'px;' );	
+}
